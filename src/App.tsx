3d16e0d2d9e66018d961a77fffc18aa2af572f34
Pawełek Kamil <kamil@nasze-wlasne.pl>
2022-07-05 11:16:07 +0200

    finished header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import ListWrapper from './components/ListWrapper/ListWrapper'
 import Toolbar from './components/MaterialUI/Toolbar/Toolbar'
 
 function App() {
@@ -8,9 +7,8 @@ function App() {
 			<header className='App-header'>
 				<Toolbar />
 			</header>
-			<ListWrapper />
 		</div>
 	)
 }
 
-export default App;
+export default App
