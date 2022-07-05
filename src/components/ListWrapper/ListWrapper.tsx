48d6b854b737faed5f4e05a6c9ab83c9823aa43e
Pawełek Kamil <kamil@nasze-wlasne.pl>
2022-07-05 16:09:07 +0200

    adding interface for useState

--- src/components/ListWrapper/ListWrapper.tsx
+++ src/components/ListWrapper/ListWrapper.tsx
@@ -11,19 +11,28 @@
 
 // export default ListWrapper
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ListItem from './ListItem/ListItem'
+
+interface IListItem {
+	id: number,
+	email: string,
+	first_name: string,
+	last_name: string,
+	avatar: string
+}
 
 const ListWrapper = () => {
 	const URL = 'https://reqres.in/api/users/'
 
-	const [users, setUsers] = React.useState([])
+	const [users, setUsers] = useState<IListItem[]>([])
+	console.log(users);
 	const usersList = async () => {
 		const res = await fetch(URL)
 		const json = await res.json()
 		setUsers(json.data)
 	}
-	React.useEffect(() => {
+	useEffect(() => {
 		usersList()
 	}, [])
 
@@ -31,6 +40,7 @@ const ListWrapper = () => {
 		<ul className='ulList'>
 			{users.length &&
 				users.map(user => {
+					console.log(user);
 					return <ListItem key={user.id} avatar={user.avatar} name={user.first_name} email={user.email} />
 				})}
 		</ul>
