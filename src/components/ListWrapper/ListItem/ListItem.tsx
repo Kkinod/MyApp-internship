859b2af16796f949c35bfac5800b370872bba1b4
Pawełek Kamil <kamil@nasze-wlasne.pl>
2022-07-05 14:51:30 +0200

    adding API

--- src/components/ListWrapper/ListItem/ListItem.tsx
+++ src/components/ListWrapper/ListItem/ListItem.tsx
@@ -1,10 +1,27 @@
 import React from 'react'
-import Card from '../../MaterialUI/ItemCard/ItemCard'
+// import Card from '../../MaterialUI/ItemCard/ItemCard'
+import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@mui/material'
 
-const ListItem = () => {
+
+const ListItem = (props: {name: string, email: string, avatar: string}) => {
 	return (
 		<li className='liItem'>
-			<Card />
+			<Card sx={{ maxWidth: 250 }}>
+				<CardMedia component='img' alt='User img' height='250' image={props.avatar} />
+				<CardContent>
+					<Typography gutterBottom variant='h5' component='div'>
+						{props.name}
+					</Typography>
+					<Typography variant='body2' color='text.secondary'>
+						{props.email}
+					</Typography>
+				</CardContent>
+				<CardActions>
+					<Button variant='contained' size='small'>
+						Details
+					</Button>
+				</CardActions>
+			</Card>
 		</li>
 	)
 }
--- src/components/ListWrapper/ListWrapper.tsx
+++ src/components/ListWrapper/ListWrapper.tsx
@@ -1,12 +1,68 @@
+// import React from 'react'
+// import ListItem from './ListItem/ListItem'
+
+// const ListWrapper = () => {
+// 	return (
+// 		<ul className='ulList'>
+// 			<ListItem />
+// 		</ul>
+// 	)
+// }
+
+// export default ListWrapper
+
 import React from 'react'
 import ListItem from './ListItem/ListItem'
 
 const ListWrapper = () => {
+	const URL = 'https://reqres.in/api/users/'
+
+	const [users, setUsers] = React.useState([])
+	const usersList = async () => {
+		const res = await fetch(URL)
+		const json = await res.json()
+		setUsers(json.data)
+	}
+	React.useEffect(() => {
+		usersList()
+	}, [])
+
 	return (
 		<ul className='ulList'>
-			<ListItem />
+			{users.length &&
+				users.map(user => {
+					return <ListItem key={user.id} avatar={user.avatar} name={user.first_name} email={user.email} />
+				})}
 		</ul>
 	)
 }
 
 export default ListWrapper
+
+// import React from 'react'
+// import ListItem from './ListItem/ListItem'
+
+// const ListWrapper = () => {
+//  const URL = 'https://reqres.in/api/users/'
+
+//  const [users, setUsers] = React.useState([])
+//  const f = async () => {
+//      const res = await fetch(URL)
+//      const json = await res.json()
+//      setUsers(json.data)
+//  }
+//  React.useEffect(() => {
+//      f()
+//  }, [])
+
+//  return (
+//      <ul className='ulList'>
+//          <ListItem />
+//          {users.map(user => {
+//                  return <ListItem key={user.id} name={user.first_name} email={user.email} avatar={user.avatar} />
+//              })}
+//      </ul>
+//  )
+// }
+
+// export default ListWrapper
