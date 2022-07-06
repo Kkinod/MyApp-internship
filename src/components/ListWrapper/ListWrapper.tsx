c4201e45d9ff4293bd5537db4ecee520c5bcab0c
Pawełek Kamil <kamil@nasze-wlasne.pl>
2022-07-06 08:03:49 +0200

    adding loading button

--- src/components/ListWrapper/ListWrapper.tsx
+++ src/components/ListWrapper/ListWrapper.tsx
@@ -13,6 +13,7 @@
 
 import React, { useEffect, useState } from 'react'
 import ListItem from './ListItem/ListItem'
+import LoadingButton from '@mui/lab/LoadingButton'
 
 interface IListItem {
 	id: number
@@ -26,11 +27,13 @@ const ListWrapper = () => {
 	const URL = 'https://reqres.in/api/users/'
 
 	const [users, setUsers] = useState<IListItem[]>([])
+
 	const usersList = async () => {
 		const res = await fetch(URL)
-		if(!res.ok) {
-			const msg = `USERS NOT FOUND: ${res.status}`;
-			throw new Error(msg);
+
+		if (!res.ok) {
+			const msg = `USERS NOT FOUND: ${res.status}`
+			throw new Error(msg)
 		}
 		const json = await res.json()
 		setUsers(json.data)
@@ -42,6 +45,9 @@ const ListWrapper = () => {
 
 	return (
 		<ul className='ulList'>
+			<LoadingButton loading variant='outlined'>
+				Submit
+			</LoadingButton>
 			{users.length &&
 				users.map(user => {
 					return <ListItem key={user.id} {...user} />
