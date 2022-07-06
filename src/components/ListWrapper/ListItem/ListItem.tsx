2a1c615cfd9f516cec0c25413481364e41b1c1cc
Pawełek Kamil <kamil@nasze-wlasne.pl>
2022-07-06 13:12:07 +0200

    add Pagination

--- src/components/ListWrapper/ListItem/ListItem.tsx
+++ src/components/ListWrapper/ListItem/ListItem.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Card from '../../MaterialUI/ItemCard/ItemCard'
-// import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@mui/material'
 
 interface IUserProps {
 	email: string
@@ -11,31 +10,9 @@ interface IUserProps {
 const ListItem = (props: IUserProps) => {
 	return (
 		<li className='liItem'>
-			<Card  {...props} />
+			<Card {...props} />
 		</li>
 	)
 }
-// const ListItem = ({ avatar, first_name, email }: IUserProps) => {
-// 	return (
-// 		<li className='liItem'>
-// 			<Card sx={{ maxWidth: 250 }}>
-// 				<CardMedia component='img' alt='User img' height='250' image={avatar} />
-// 				<CardContent>
-// 					<Typography gutterBottom variant='h5' component='div'>
-// 						{first_name}
-// 					</Typography>
-// 					<Typography variant='body2' color='text.secondary'>
-// 						{email}
-// 					</Typography>
-// 				</CardContent>
-// 				<CardActions>
-// 					<Button variant='contained' size='small'>
-// 						Details
-// 					</Button>
-// 				</CardActions>
-// 			</Card>
-// 		</li>
-// 	)
-// }
 
 export default ListItem
--- src/components/ListWrapper/ListWrapper.tsx
+++ src/components/ListWrapper/ListWrapper.tsx
@@ -14,6 +14,7 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import ListItem from './ListItem/ListItem'
+import Pagination from '@mui/material/Pagination'
 import LoadingButton from '@mui/lab/LoadingButton'
 
 interface IListItem {
@@ -26,34 +27,44 @@ interface IListItem {
 
 const ListWrapper = () => {
 	const [users, setUsers] = useState<IListItem[]>([])
-	const URL = 'https://reqres.in/api/users/'
+	const [totalPages, setPages] = useState<number>(0)
+	const [page, setPage] = useState<number>(1)
+	const URL = `https://reqres.in/api/users?page=${page}`
 
 	const usersList = async () => {
 		const res = await axios.get(URL)
 
 		if (!(res.status === 200)) {
 			const msg = `USERS NOT FOUND: ${res.status}`
-			throw new Error(msg)
+			throw alert(msg)
 		}
 
 		setUsers(res.data.data)
+		setPages(res.data.total_pages)
+		setPage(res.data.page)
+		console.log(res.data)
 	}
 
 	useEffect(() => {
 		usersList()
-	}, [])
+	}, [page])
 
 	return (
 		<ul className='ulList'>
-			{users.length ? (
-				users.map(user => {
-					return <ListItem key={user.id} {...user} />
-				})
-			) : (
-				<LoadingButton loading variant='outlined'>
-					Loading
-				</LoadingButton>
-			)}
+			<div className='usersList'>
+				{totalPages >= 1 ? (
+					users.length ? (
+						users.map(user => {
+							return <ListItem key={user.id} {...user} />
+						})
+					) : (
+						<LoadingButton loading variant='outlined'>
+							Loading
+						</LoadingButton>
+					)
+				) : undefined}
+			</div>
+			<Pagination count={totalPages} onChange={(e, page) => setPage(page)}></Pagination>
 		</ul>
 	)
 }
