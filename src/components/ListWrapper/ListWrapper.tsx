257e643a3bb8f74a18d6680430ff932866f71ab8
Pawełek Kamil <kamil@nasze-wlasne.pl>
2022-07-07 21:39:31 +0200

    adding user search filter

--- src/components/ListWrapper/ListWrapper.tsx
+++ src/components/ListWrapper/ListWrapper.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useEffect, useState, useReducer, ChangeEvent } from 'react'
+import React, { createContext, useEffect, useState, ChangeEvent } from 'react'
 import axios from 'axios'
 import ListItem from './ListItem/ListItem'
 import Pagination from '@mui/material/Pagination'
@@ -15,12 +15,10 @@ interface IListItem {
 export const UsersContext = createContext<IListItem>({} as IListItem)
 
 const ListWrapper = () => {
-	const [searchContent, setSearchInputContent] = useState<string>('Search...')
-	// const [inputContent, setInputContent] = useReducer((state, newState) => ({...state, ...newState}), {
-	// 	searchInputContent: 'Search'
-	// })
-
+	const [searchContent, setSearchInputContent] = useState<string>('')
 	const [users, setUsers] = useState<IListItem[]>([])
+	const [usersFiltered, setUsersFiltered] = useState<IListItem[]>([])
+
 	const [totalPages, setPages] = useState(0)
 	const [page, setPage] = useState(1)
 	const URL = `https://reqres.in/api/users?page=${page}`
@@ -40,11 +38,53 @@ const ListWrapper = () => {
 		}
 
 		usersList()
-	}, [page])
+	}, [page, URL])
 
 	const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
 		setSearchInputContent(e.target.value)
 	}
+
+	// const resultOfSearch = users.filter(user => {
+	// 	return (
+	// 		user.first_name.toLowerCase().includes(searchContent.toLowerCase()) ||
+	// 		user.email.toLowerCase().includes(searchContent.toLowerCase())
+	// 	)
+	// })
+
+	useEffect(() => {
+		setUsersFiltered(
+			users.filter(
+				user =>
+					user.first_name.toLowerCase().includes(searchContent.toLowerCase()) ||
+					user.email.toLowerCase().includes(searchContent.toLowerCase())
+			)
+		)
+		// setUsers(usersFiltered)
+	}, [searchContent, users])
+	//----------------------------
+	// useEffect(() => {
+	// 	const resultOfSearch = users.filter(
+	// 		user =>
+	// 			user.first_name.toLowerCase().includes(searchContent.toLowerCase()) ||
+	// 			user.email.toLowerCase().includes(searchContent.toLowerCase())
+	// 	)
+	// 	setUsers(resultOfSearch)
+	// }, [searchContent])
+	//----------------------------------
+	// useEffect(() => {
+	// 	const name = () => {
+
+	// 		const resultOfSearch = () =>  users.filter(
+	// 			user =>
+	// 				user.first_name.toLowerCase().includes(searchContent.toLowerCase()) ||
+	// 				user.email.toLowerCase().includes(searchContent.toLowerCase())
+	// 		);
+	// 		setUsers(resultOfSearch)
+	// 	}
+
+	// 	name()
+
+	// }, [searchContent])
 
 	return (
 		<div className='container'>
@@ -57,9 +97,12 @@ const ListWrapper = () => {
 			<ul className='ulList'>
 				{totalPages >= 1 ? (
 					users.length ? (
-						users
-							// .filter(item => item.first_name.toLowerCase().includes(searchContent.toLowerCase()) && item.email.toLowerCase().includes(searchContent.toLowerCase()))
-							.filter(item => item.first_name.toLowerCase().includes(searchContent.toLowerCase()) || item.email.toLowerCase().includes(searchContent.toLowerCase()))
+						usersFiltered
+							// .filter(
+							// 	item =>
+							// 		item.first_name.toLowerCase().includes(searchContent.toLowerCase()) ||
+							// 		item.email.toLowerCase().includes(searchContent.toLowerCase())
+							// )
 							.map(({ id, avatar, first_name, last_name, email }: IListItem) => {
 								return (
 									<UsersContext.Provider key={id} value={{ id, email, first_name, last_name, avatar }}>
